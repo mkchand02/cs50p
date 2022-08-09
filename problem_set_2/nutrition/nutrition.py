ca96c3e34a8be48ec09e8dbca0bc30de1a3d218e
mkchand02 <mc99chand@gmail.com>
2022-08-09 04:00:05 +0000

    Update and submit nutrition.py ps2 problem

--- problem_set_2/nutrition/nutrition.py
+++ problem_set_2/nutrition/nutrition.py
@@ -1,3 +1,29 @@
 """
 implement a program that prompts users to input a fruit (case-insensitively) and then outputs the number of calories in one portion of that fruit, per the FDA’s poster for fruits, which is also available as text. Capitalization aside, assume that users will input fruits exactly as written in the poster (e.g., strawberries, not strawberry). Ignore any input that isn’t a fruit.
 """
+calories = {
+    "Apple": 130,
+    "Avocado": 50,
+    "Banana": 110,
+    "Cantaloupe": 50,
+    "Grapefruit": 60,
+    "Grapes": 90,
+    "Honeydew Melon": 50,
+    "Kiwifruit": 90,
+    "Lemon": 15,
+    "Lime": 20,
+    "Nectarine": 60,
+    "Orange": 80,
+    "Peach": 60,
+    "Pear": 100,
+    "Pineapple": 50,
+    "Plums": 70,
+    "Strawberries": 50,
+    "Sweet Cherries": 100,
+    "Tangerine": 50,
+    "Watermelon": 80
+}
+
+fruit = input("Item:").title().strip()
+if fruit in calories:
+    print("Calories:", calories[fruit])
