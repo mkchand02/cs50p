b37c9c9100ded53ba1a13c1ea8442ff9ee41fab4
mkchand02 <mc99chand@gmail.com>
2022-08-09 05:09:15 +0000

    Create files for ps3 and update and submit grocery.py

---
+++ problem_set_3/outdated/outdated.py
@@ -0,0 +1,3 @@
+"""
+implement a program that prompts the user for a date, anno Domini, in month-day-year order, formatted like 9/8/1636 or September 8, 1636, wherein the month in the latter might be any of the values in the list below:
+"""
