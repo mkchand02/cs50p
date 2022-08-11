2c12ba0f3a1d8cb0618e304660b3095a31cacd37
mkchand02 <mc99chand@gmail.com>
2022-08-11 11:31:52 +0000

    update and submit game.py of ps4

--- problem_set_4/game/game.py
+++ problem_set_4/game/game.py
@@ -7,3 +7,36 @@ If the guess is smaller than that integer, the program should output Too small! 
 If the guess is larger than that integer, the program should output Too large! and prompt the user again.
 If the guess is the same as that integer, the program should output Just right! and exit.
 """
+
+import random
+
+while True:
+    #this loop will get the level from user and terminate on successfully getting a valid level.
+    level = input("Level :")
+    try:
+        level = int(level)
+        if level <= 0:
+            continue
+        else:
+            answer = random.randint(1, level)
+            break
+    except ValueError:
+        pass
+
+while True:
+    #this loop will get the Guess from user and terminate on successfully getting a guess that is just right.
+    guess_no = input("Guess :")
+    try:
+        guess_no = int(guess_no)
+        if guess_no <= 0:
+            continue
+        else:
+            if guess_no < answer:
+                print("Too small!")
+            elif guess_no > answer:
+                print("Too large!")
+            else:
+                print("Just right!")
+                break
+    except ValueError:
+        pass
