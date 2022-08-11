5bff7785ba22ae5a4cfc98d7a78e0198d635a949
mkchand02 <mc99chand@gmail.com>
2022-08-11 11:09:19 +0000

    update and submit figlet.py of ps4

--- problem_set_4/figlet/figlet.py
+++ problem_set_4/figlet/figlet.py
@@ -17,3 +17,37 @@ Prompts the user for a str of text.
 Outputs that text in the desired font.
 If the user provides two command-line arguments and the first is not -f or --font or the second is not the name of a font, the program should exit via sys.exit with an error message.
 """
+
+import sys, random
+from pyfiglet import Figlet
+
+
+def stylish_print(figlet, font):
+    """Stylish print using a printer object. Ex. : figlet"""
+    figlet.setFont(font=font)
+    sentence = input("enter :")
+    print(figlet.renderText(sentence))
+
+
+def handle_invalid_usage():
+    """Print Error Message and exits program"""
+    print("Invalid Usage")
+    sys.exit(-1)
+
+
+figlet = Figlet()
+if len(sys.argv) == 1:
+    random_font = random.choice(figlet.getFonts())
+    stylish_print(figlet, random_font)
+elif len(sys.argv) == 3:
+    if sys.argv[1] == '-f' or sys.argv[1] == '--font':
+        if sys.argv[2] in figlet.getFonts(
+        ):  #Check if valid font or not, i.e., if font exists in figlet class
+            stylish_print(figlet, sys.argv[2])
+        else:  #If not valid font, handle invalid usage
+            handle_invalid_usage()
+    else:
+        handle_invalid_usage()
+
+else:  #Given irrelevant arguments which should prompt invalid usage
+    handle_invalid_usage()
