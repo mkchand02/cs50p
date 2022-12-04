a0b679060076bec7dab702287f8924fb87086378
mkchand02 <mc99chand@gmail.com>
2022-12-04 03:58:27 +0000

    Update and submit response.py as part of cs50p pset7

--- problem_set_7/response/response.py
+++ problem_set_7/response/response.py
@@ -1,3 +1,15 @@
 """
 In a file called response.py, using either validator-collection or validators from PyPI, implement a program that prompts the user for an email address via input and then prints Valid or Invalid, respectively, if the input is a syntatically valid email address. You may not use re. And do not validate whether the email address’s domain name actually exists.
 """
+
+import validator_collection as validator
+
+def main():
+    email = input("What's your email address?")
+    if validator.is_email(email):
+        print("Valid")
+    else:
+        print("Invalid")
+
+if __name__ == "__main__":
+    main()
