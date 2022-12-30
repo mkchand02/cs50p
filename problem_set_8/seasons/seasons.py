eee0836f882d8901803e9ff89e5cc3eed34199d9
mkchand02 <mc99chand@gmail.com>
2022-12-30 10:25:35 +0000

    update and submit seasons.py and test_seasons.py as part of cs50p pset8

--- problem_set_8/seasons/seasons.py
+++ problem_set_8/seasons/seasons.py
@@ -3,13 +3,53 @@ In a file called seasons.py, implement a program that prompts the user for their
 """
 
 from datetime import date
-
+import re
+import inflect, sys
 
 def main():
-    ...
+    """driver code function"""
+    birth_date = input("Date of Birth: ")
+    try:
+        print(date_to_mins(birth_date))
+    except ValueError:
+        sys.exit("Invalid Date")
 
+def date_to_mins(date_str, date_2_str = None):
+    """returns the time duration in minutes between the dates provided in the format "YYYY-MM-DD" as str datatype. If date_2_str is not provided then , we assume time as today's(current) date. Anything else as input param throws ValueError
 
-...
+    @param date_str - str input date provided in the format "YYYY-MM-DD". 
+    @param date_2_str - str input date provided in the format "YYYY-MM-DD". 
+    @return - the time duration in minutes between the date provided in the format "YYYY-MM-DD" as str datatype and today's(current) date.
+    @raises ValueError - Invalid Date/Provided Date in Incorrect Format
+    """
+    date_str = date_str.strip()
+    # check if data is in valid format
+    # match = re.match(r"^\d{4}-[0-1]\d-[0-3]\d$",date_str)
+    # if match:
+    # split date into year, month and days respectively
+    year, month, day = date_str.split("-")
+    # convert str to int
+    year, month, day = int(year), int(month), int(day)
+    
+    target_date = date(year, month, day) # may raise ValueError if any of the params provided are invalid
+    if date_2_str:
+        year2, month2, day2 = date_2_str.split("-")
+        year2, month2, day2 = int(year2), int(month2), int(day2)
+        today = date(year2, month2, day2)
+    else:
+        today = date.today() # get today's date
+    time_duration = today - target_date # get the time diff.
+    # get the time in secs and convert to mins
+    mins = int(time_duration.total_seconds() / 60)
+    # setup inflect for converting to mins
+    p = inflect.engine()
+    words = p.number_to_words(mins, andword="")
+    # first alphabet of the string should be upper-case
+    words = words.capitalize() + " minutes"
+    return words
+    
+    
+    
 
 
 if __name__ == "__main__":
--- problem_set_8/seasons/test_seasons.py
+++ problem_set_8/seasons/test_seasons.py
@@ -1,3 +1,25 @@
 """
 implement, in a file called test_seasons.py, one or more functions that test your implementation of any functions besides main in seasons.py thoroughly
 """
+
+from seasons import date_to_mins
+import pytest
+
+def test_invalid_time():
+    with pytest.raises(ValueError):
+        date_to_mins("gwefwgre")
+    with pytest.raises(ValueError):
+        date_to_mins("2022-31-21")
+    with pytest.raises(ValueError):
+        date_to_mins("2022-21-21-12")
+    with pytest.raises(ValueError):
+        date_to_mins("January 1, 1999")
+    with pytest.raises(ValueError):
+        date_to_mins("2022-21-21", "January 1, 1999")
+
+def test_valid_time():
+    assert date_to_mins("2022-12-15", "2022-12-16") == "One thousand, four hundred forty minutes"
+    assert date_to_mins("1970-01-01", "2000-01-01") == "Fifteen million, seven hundred seventy-eight thousand eighty minutes"
+    
+    assert date_to_mins("1999-01-01", "2000-01-01") == "Five hundred twenty-five thousand, six hundred minutes"
+    
